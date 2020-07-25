91490c5f1e1aa78aa61ebf11219dd3334ffb054c
kwonsw055 <kwonsw055@gmail.com>
2020-07-25 15:53:24 +0900

    test use of glide

--- Frontend/app/src/main/java/com/freefriday/hillo/RVAdapter.kt
+++ Frontend/app/src/main/java/com/freefriday/hillo/RVAdapter.kt
@@ -7,22 +7,43 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.GlideBuilder
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.module.AppGlideModule
+import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.signature.ObjectKey
 
-
+@GlideModule
+class GlideModule : AppGlideModule() {
+    override fun applyOptions(context: Context, builder: GlideBuilder) {
+        super.applyOptions(context, builder)
+        builder.apply { RequestOptions().diskCacheStrategy(DiskCacheStrategy.ALL).signature(
+            ObjectKey(System.currentTimeMillis().toShort())
+        ) }
+    }
+}
 
 class FreetimeRVAdapter(var data:MutableList<Freetime>?) : RecyclerView.Adapter<FreetimeRVAdapter.RVHolder>() {
     lateinit var context: Context
-    val cache:MutableMap<Long, String> = mutableMapOf()
+    val namecache:MutableMap<Long, String> = mutableMapOf()
+    val urlcache:MutableMap<Long, String?> = mutableMapOf()
     class RVHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val text_name = itemView.findViewById<TextView>(R.id.recycler_name)
         val text_time = itemView.findViewById<TextView>(R.id.recycler_time)
         val btn_start = itemView.findViewById<Button>(R.id.recycler_recstart)
         val btn_del = itemView.findViewById<Button>(R.id.recycler_recdel)
+        val img_rec = itemView.findViewById<ImageView>(R.id.recycler_image)
         fun setText(name: String?, time: Timepair){
             text_name.text = name
             text_time.text = time.toString()
+        }
+        fun setImg(url:String?){
+            GlideApp.with(img_rec).load(url).error(R.drawable.ic_launcher_foreground).into(img_rec)
         }
     }
 
@@ -35,16 +56,22 @@ class FreetimeRVAdapter(var data:MutableList<Freetime>?) : RecyclerView.Adapter<
     override fun getItemCount(): Int = data?.size ?: 0
 
     override fun onBindViewHolder(holder: RVHolder, position: Int) {
-        if(!cache.containsKey(data!![position].id)){
+        if(!(namecache.containsKey(data!![position].id)&&urlcache.containsKey(data!![position].id))){
             getFriend(context, data!![position].id,{f:Friend?->
                 (context as Activity).runOnUiThread{
                     if(data!!.size>position){
-                        cache[data!![position].id] = f?.nickname?:context.getString(R.string.name_unknown)
+                        namecache[data!![position].id] = f?.nickname?:context.getString(R.string.name_unknown)
+                        urlcache[data!![position].id] = f?.url
+                        holder.setText(namecache[data!![position].id], data!![position].time)
+                        holder.setImg(urlcache[data!![position].id])
                     }
                 }
             })
         }
-        holder.setText(cache[data!![position].id], data!![position].time)
+        else{
+            holder.setText(namecache[data!![position].id], data!![position].time)
+            holder.setImg(urlcache[data!![position].id])
+        }
         holder.btn_del.setOnClickListener { deleteData(position) }
     }
 
